4f15797df2fb09991b9380ae816898028af4777d
darkenvy <darkenvy6@gmail.com>
2016-09-15 20:34:21 -0700

    initial commit

---
+++ app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {AjaxComponent} from './ajax.component';
+
+@Component({
+  directives: [AjaxComponent],
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(public navCtrl: NavController) {
+
+  }
+}
